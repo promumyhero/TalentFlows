400813b6bb6726e600c5334e0d81c1340df5411b
Ridho Hery Winarto <2009041@uwhs.ac.id>
2025-02-12 23:15:45 +0700

    feat: add UserTypeSelection component with building and user icons

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import { OnboardingForm } from "@/components/forms/onboarding/OnboardingForm";
+
+export default function OnboardingPage() {
+    return (
+        <div className="min-h-screen w-screen py-10 flex flex-col items-center justify-center">
+            <OnboardingForm />
+        </div>
+    )
+}
