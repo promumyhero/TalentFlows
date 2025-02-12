3c094f6a067e3a4aec922e40b50a466d4044f3ce
Ridho Hery Winarto <2009041@uwhs.ac.id>
2025-02-12 15:06:49 +0700

    memproses loginform

---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png"
+import { LoginForm } from "@/components/forms/LoginForm";
+
+export default function Login() {
+    return (
+        <div className="min-h-screen w-screen flex items-center justify-center">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <Link href={"/"} className="flex items-center gap-2 self-center">
+                    <Image src={Logo} alt="logo" className="size-10"/>
+                    <h1 className="text-2xl font-bold">Talent<span className="text-primary">Flow</span></h1>
+                </Link>
+                <LoginForm />
+            </div>
+        </div>
+    )
+}
